cec8738489951857aa6f2d9e57c322c282abf986
Andy <andyfilchev@gmail.com>
2018-04-23 18:39:55 +0300

    feat: Add Stopwatch class

---
+++ js/stopwatch.js
@@ -0,0 +1,52 @@
+function Stopwatch(elem) {
+    let time = 0;
+    let interval, offset;
+    this.isOn = false;
+
+    function update(){
+        time += delta();
+        let formattedTime  = timeFormatter(time);
+        elem.textContent = formattedTime;
+    };
+
+    function delta(){
+        let now = Date.now();
+        let timePassed = now - offset;
+        offset = now;
+        return timePassed;
+    };
+
+    function pad(t) {
+        return ('00' + t).substr(-2);
+    }
+
+    function timeFormatter(timeInMilliseconds){
+        let time = new Date(timeInMilliseconds);
+        let minutes = pad(time.getMinutes());
+        let seconds = pad(time.getSeconds());
+        let milliseconds = pad(Math.floor(time.getMilliseconds() / 10));
+        return minutes + ":" + seconds + ":" + milliseconds;
+    };
+
+
+
+    this.start = function() {
+        if(!this.isOn) {
+            interval = setInterval(update, 1);
+            offset = Date.now();
+            this.isOn = true;
+        }
+    };
+
+    this.stop = function() {
+        if(this.isOn){
+            clearInterval(interval);
+            interval = null;
+            this.isOn = false;
+        }
+    };
+
+    this.reset = function() {
+        time = 0;
+    };
+}
